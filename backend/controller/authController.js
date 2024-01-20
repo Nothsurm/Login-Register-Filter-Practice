9e936877ad37d781b94cdfc76d76363a686cb165
Mike Rushton <mrush94ton@gmail.com>
2024-01-20 21:01:40 +0000

    created middleware functionality

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,30 +1,33 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../middleware/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
-        res.send('Please fill all inputs')
+        next(errorHandler(400, 'Please fill in all the inputs'))
     }
 
     if (username && username.length < 5) {
-        res.send('Please use more than 7 characters for your username')
+        next(errorHandler(400, 'Please use more than 7 characters for your username'))
     }
     if (password && password.length < 5) {
-        res.send('Please use more than 7 characters for your password')
+        next(errorHandler(400, 'Please use more than 7 characters for your password'))
     }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10)
 
     const newUser = new User({
         username,
         email,
-        password
+        password: hashedPassword
     })
 
     try {
         await newUser.save()
         res.json('Signup successfull')
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 }
