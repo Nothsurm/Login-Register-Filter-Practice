76c4e5cfa83dcb17803eb35b0afcfd41ae5908c4
Mike Rushton <mrush94ton@gmail.com>
2024-01-20 15:49:10 +0000

    created user model

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,3 +1,18 @@
+import User from "../models/userModel.js";
+
 export const signup = async (req, res) => {
-    res.send('Hello signup')
+    const { username, email, password } = req.body;
+
+    const newUser = new User({
+        username,
+        email,
+        password
+    })
+
+    try {
+        await newUser.save()
+        res.json('Signup successfull')
+    } catch (error) {
+        console.log(error)
+    }
 }
