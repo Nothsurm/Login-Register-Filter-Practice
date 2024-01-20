52e3b063f9d595dfb699318678647854c820f5df
Mike Rushton <mrush94ton@gmail.com>
2024-01-20 20:53:30 +0000

    connected to database

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,7 +1,19 @@
 import User from "../models/userModel.js";
+import bcryptjs from 'bcryptjs';
 
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
+
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        res.send('Please fill all inputs')
+    }
+
+    if (username && username.length < 5) {
+        res.send('Please use more than 7 characters for your username')
+    }
+    if (password && password.length < 5) {
+        res.send('Please use more than 7 characters for your password')
+    }
 
     const newUser = new User({
         username,
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import authRoute from './routes/authRoute.js'
 
 const app = express()
+
+app.use(express.json())
+
+dotenv.config()
+
+mongoose.connect(process.env.VITE_MONGODB).then(() => {
+    console.log('Connected to DB')
+}).catch((err) => {
+    console.log(err)
+})
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000')
