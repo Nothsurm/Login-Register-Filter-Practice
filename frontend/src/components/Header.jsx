04ca148cf549f3637dbe2fe55ca9c035ef03b6e1
Mike Rushton <mrush94ton@gmail.com>
2024-01-21 22:13:03 +0000

    created header component and added redux store to the components tree

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user)
+  
+
   return (
-    <div>Header</div>
+    <div className='flex justify-center mt-5'>
+      { currentUser ? (
+        <div className="flex flex-row flex-wrap">
+          <p>Current User logged in:</p> {"  "} <span className='ml-2 text-blue-600'>{currentUser.username}</span>
+        </div>
+      ) : (
+        <>
+          <p>No User currently logged in</p>
+        </>
+      )}
+    </div>
   )
 }
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,12 +1,15 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { signInFailure, signInStart, signInSuccess } from "../redux/user/userSlice";
+import Loader from "../components/Loader";
 
 export default function SignIn() {
     const [formData, setFormData] = useState('')
-    const [errorMessage, setErrorMessage] = useState(null)
-    const [loading, setLoading] = useState(false)
+    const { loading, error } = useSelector((state) => state.user)
 
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const handleChange = (e) => {
         setFormData({
@@ -22,9 +25,7 @@ export default function SignIn() {
         }
 
         try {
-            setLoading(true)
-            setErrorMessage(null)
-            setTimeout(() => setLoading(false), 2000)
+            dispatch(signInStart())
             const result = await fetch('/api/auth/signin', {
                 method: 'POST',
                 headers: {
@@ -34,13 +35,12 @@ export default function SignIn() {
             })
             const data = await result.json()
             if (data.success === false) {
-                return setErrorMessage(data.message)
+                dispatch(signInFailure(data.message))
             }
-            setLoading(false)
+            dispatch(signInSuccess(data))
             navigate('/products')
         } catch (error) {
-            setLoading(false)
-            setErrorMessage(data.message)
+            dispatch(signInFailure(error.message))
         }
     }
 
@@ -73,13 +73,16 @@ export default function SignIn() {
                         type='submit'
                         className='px-4 py-2 border rounded-lg bg-blue-300 hover:bg-blue-400 transform duration-150'
                     >
-                        Sign In
+                        { loading ? <Loader/> : 'Sign In'}
                     </button>
                 </div>
                 <div className="">
                     Don't have an account? <Link to='/' className='text-blue-500 hover:underline'>Register</Link>
                 </div>
             </form>
+            { error && (
+                <div className="mt-5 text-lg bg-red-500 p-2 rounded-lg">{error}</div>
+            ) }
         </div>
     </div>
   )
