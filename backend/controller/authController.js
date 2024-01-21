315958954714ce9bce5babf04515e77d0c73b8e3
Mike Rushton <mrush94ton@gmail.com>
2024-01-21 15:34:44 +0000

    created signin api route and functionality

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/userModel.js";
-import bcryptjs from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import { errorHandler } from "../middleware/error.js";
 
 export const signup = async (req, res, next) => {
@@ -16,7 +16,7 @@ export const signup = async (req, res, next) => {
         next(errorHandler(400, 'Please use more than 7 characters for your password'))
     }
 
-    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const hashedPassword = bcrypt.hashSync(password, 10)
 
     const newUser = new User({
         username,
@@ -29,5 +29,33 @@ export const signup = async (req, res, next) => {
         res.json('Signup successfull')
     } catch (error) {
         next(error)
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body
+
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'Please fill in all the inputs'))
+    }
+
+    if (password && password.length < 5) {
+        next(errorHandler(400, 'Your password must contain more than 5 characters'))
+    }
+
+    const existingUser = await User.findOne({email})
+
+    if (existingUser) {
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password)
+
+        if (!isPasswordValid) {
+            next(errorHandler(400, 'Incorrect password, please try again'))
+        } else {
+            res.status(201).json({
+                _id: existingUser._id,
+                username: existingUser.username, 
+                email: existingUser.email, 
+            })
+        }
     }
 }
