f3bcaec7f4a86197eee62a15dc6bcc788473df8e
Mike Rushton <mrush94ton@gmail.com>
2024-01-22 13:07:19 +0000

    added signin and register components to header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
 import { Toaster } from "react-hot-toast";
+import Products from "./pages/Products";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/signin' element={<SignIn/>}></Route>
+        <Route path='/products' element={<Products/>}></Route>
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,47 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom';
+import { signOutUserFailure, signOutUserStart, signOutUserSuccess } from '../redux/user/userSlice';
+import toast from 'react-hot-toast';
+import Loader from './Loader';
 
 export default function Header() {
-  const { currentUser } = useSelector((state) => state.user)
-  
+  const { currentUser, loading } = useSelector((state) => state.user)
 
+  const dispatch = useDispatch();
+
+  const handleSignOut = async () => {
+    dispatch(signOutUserStart())
+    const res = await fetch('/api/auth/signout')
+    const data = res.json()
+
+    if (data.success = false) {
+      dispatch(signOutUserFailure(data))
+      toast.error('Unable to logout, please try again')
+      return;
+    }
+    dispatch(signOutUserSuccess())
+    toast.success('User logged out')
+  }
+  
   return (
     <div className='flex justify-center mt-5'>
       { currentUser ? (
-        <div className="flex flex-row flex-wrap">
-          <p>Current User logged in:</p> {"  "} <span className='ml-2 text-blue-600'>{currentUser.username}</span>
+        <div className="flex flex-row flex-wrap gap-5">
+          <p>Current User logged in:</p> {"  "} <span className='text-blue-600'>{currentUser.username}</span> 
+          <button 
+            className='ml-2 rounded-lg border px-4 py-2'
+            onClick={handleSignOut}
+          >
+            { loading ? <Loader/> : 'Sign Out'}
+          </button>
         </div>
       ) : (
-        <>
+        <div className='flex flex-row gap-5'>
           <p>No User currently logged in</p>
-        </>
+          <Link to='/signin' className='text-red-400 hover:underline'>Sign In</Link>
+          <Link to='/' className='text-blue-400 hover:underline'>Register</Link>
+        </div>
       )}
     </div>
   )
