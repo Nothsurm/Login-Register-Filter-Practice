ce928676a40df2af2a162950409b8266fe7e1d5f
Mike Rushton <mrush94ton@gmail.com>
2024-01-23 16:10:13 +0000

    created Listing model

---
+++ backend/models/listingModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const listingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    }
+})
+
+const Listing = mongoose.model('Listing', listingSchema)
+
+export default Listing;
